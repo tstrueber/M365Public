1114dae2209ff426d9dab44cffa7e483e86f72b1
tstrueber <timo.strueber@outlook.com>
2024-01-22 08:42:20 +0100

    upload

---
+++ Exchange/Export-Forwarders.ps1
@@ -0,0 +1,29 @@
+﻿$AllMailboxes = Get-Mailbox -ResultSize Unlimited
+$InternalForwarders = $AllMailboxes | where-object { $_.ForwardingAddress -ne $null }
+$GenericForwarders = $AllMailboxes | where-object { $_.ForwardingSmtpAddress -ne $null }
+
+write-host "Internal Forwarders:      " $InternalForwarders.Count
+write-host "SMTP Address Forwarders:  " $GenericForwarders.Count
+
+$AllForwarders = $InternalForwarders + $GenericForwarders
+
+$export = @()
+foreach($forwarder in $AllForwarders)
+{
+    $psobject = New-Object -TypeName psobject
+    
+    $resolvedForwardingAddress = $null
+    $resolvedForwardingAddress = (Get-Recipient $forwarder.ForwardingAddress).primarysmtpaddress
+    
+    $psobject | Add-Member -MemberType NoteProperty -Name PrimarySmtpAddress -Value $forwarder.PrimarySmtpAddress
+    $psobject | Add-Member -MemberType NoteProperty -Name ForwardingAddress -Value $forwarder.ForwardingAddress
+    $psobject | Add-Member -MemberType NoteProperty -Name resolvedForwardingAddress -Value $resolvedForwardingAddress
+    $psobject | Add-Member -MemberType NoteProperty -Name ForwardingSmtpAddress -Value $forwarder.ForwardingSmtpAddress
+    $psobject | Add-Member -MemberType NoteProperty -Name DeliverToMailboxAndForward -Value $forwarder.DeliverToMailboxAndForward
+    $export += $psobject
+}
+$date = get-date -f yyyy-MM-dd
+$filename = $date + "_forwarders.json"
+$export `
+    | Select-Object PrimarySmtpAddress,ForwardingAddress,resolvedForwardingAddress,ForwardingSmtpAddress,DeliverToMailboxAndForward -Unique `
+    | ConvertTo-Json | out-file "C:\temp\$filename"
---
+++ Exchange/Import-Forwarders.ps1
@@ -0,0 +1,30 @@
+﻿Connect-ExchangeOnline
+
+$forwarders = `
+    get-content "C:temp\forwarders.json" `
+    | ConvertFrom-Json
+
+$importlist = $forwarders | Where-Object{$_.PrimarySmtpAddress -match "contoso.com" } # filter the list
+$importlist `
+    | sort-object PrimarySmtpAddress `
+    | select-object PrimarySmtpAddress,resolvedForwardingAddress,ForwardingSmtpAddress,DeliverToMailboxAndForward
+
+foreach($forwarder in $importlist)
+{
+    if($forwarder.ForwardingSmtpAddress -ne $null)
+    {
+        # when forwarding smtp address is set
+        set-mailbox $forwarder.PrimarySmtpAddress `
+            -ForwardingSmtpAddress $forwarder.ForwardingSmtpAddress `
+            -DeliverToMailboxAndForward $forwarder.DeliverToMailboxAndForward
+        Write-Host "Forwarding" $forwarder.PrimarySmtpAddress "to" $forwarder.ForwardingSmtpAddress "- DeliverToMailboxAndForward:" $forwarder.DeliverToMailboxAndForward
+    }
+    else
+    {
+        # when forwarding to a specific object is set
+        set-mailbox $forwarder.PrimarySmtpAddress `
+            -ForwardingSmtpAddress $forwarder.resolvedForwardingAddress `
+            -DeliverToMailboxAndForward $forwarder.DeliverToMailboxAndForward
+        Write-Host "Forwarding" $forwarder.PrimarySmtpAddress "to" $forwarder.resolvedForwardingAddress "- DeliverToMailboxAndForward:" $forwarder.DeliverToMailboxAndForward
+    }
+}
